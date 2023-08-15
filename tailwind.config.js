e364f7656405a5111cf6dba9a1be4cde3765bb4b
Huzi1337 <krzysztof.jagoda99@gmail.com>
2023-08-15 11:09:19 +0200

    Initial commit. Tailwind theme adjusted.

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./**/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      montserrat: ["Montserrat", "sans-serif"],
+      inter: ["Inter", "sans-serif"],
+    },
+    colors: {
+      green: "#1B5B31",
+      beige: "#DCC1AB",
+      black: "#111111",
+      grey: "#F5F0EC",
+      white: "#FFFFFF",
+    },
+  },
+  plugins: [],
+};
