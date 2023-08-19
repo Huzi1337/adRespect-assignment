001f223df41e133a6757c24060405c403bb2a40e
Huzi1337 <krzysztof.jagoda99@gmail.com>
2023-08-19 09:59:13 +0200

    Made mobile menu close on lost focus.

--- main.js
+++ main.js
@@ -2,45 +2,70 @@ import "./src/style.css";
 import toggleHidden from "./scripts/toggleDropdown";
 import setupHomePage from "./src/pages/home";
 
-const ofertaButton = document.querySelector("#oferta__btn");
-const ofertaDropdown = document.querySelector("#oferta__dropdown");
-ofertaButton.addEventListener("click", () => toggleHidden(ofertaDropdown));
-
 const navbar = document.querySelector("#navbar");
-console.log("children", navbar.children, "children Nodes", navbar.childNodes);
+const navbarOptions = navbar.querySelector("#navbarOptions");
 
 const mobileHamburger = navbar.querySelector("#hamburger");
-mobileHamburger.addEventListener("click", () => toggleHidden(navbarOptions));
+mobileHamburger.addEventListener("click", () => {
+  const booleanValue = mobileHamburger.dataset.active === "true";
+  mobileHamburger.dataset.active = !booleanValue;
+  toggleHidden(navbarOptions);
+});
+
+const hideElement = (element, event) => {
+  if (
+    !element.contains(event.target) &&
+    !element.classList.contains("hidden") &&
+    event.target != mobileHamburger &&
+    navbar.dataset.view === "mobile"
+  ) {
+    mobileHamburger.dataset.active = "false";
+    element.classList.add("hidden");
+  }
+};
 
 const toggleMobile = () => {
+  const ofertaDropdown = document.querySelector("#oferta__dropdown");
+  const navbarOptions = navbar.querySelector("#navbarOptions");
+  const ofertaElements = ofertaDropdown.querySelectorAll("li");
+
   if (window.innerWidth <= 1000 && navbar.dataset.view != "mobile") {
+    const clickListener = (event) => hideElement(navbarOptions, event);
+    window.addEventListener("click", clickListener);
     navbar.dataset.view = "mobile";
 
-    const navbarOptions = navbar.querySelector("#navbarOptions");
-    navbarOptions.className =
-      "absolute p-6 top-full right-0 flex flex-col-reverse bg-white gap-6 hidden";
-
     mobileHamburger.classList.remove("hidden");
+
+    navbarOptions.className =
+      "absolute py-5 px-10 top-full right-0 flex flex-col-reverse bg-white gap-6 hidden animate-slideDown transition-all";
+
+    ofertaElements.forEach((element) => element.classList.add("pl-10"));
+
+    ofertaDropdown.classList.add("-left-10");
+    ofertaDropdown.classList.add("-right-10");
+    ofertaDropdown.classList.remove("left-[-50%]");
   } else if (window.innerWidth > 1000 && navbar.dataset.view != "desktop") {
-    mobileHamburger.removeEventListener("click", () =>
-      toggleHidden(navbarOptions),
-    );
+    const clickListener = (event) => hideElement(navbarOptions, event);
+
+    window.removeEventListener("click", clickListener);
     navbar.dataset.view = "desktop";
-    const navbarOptions = navbar.querySelector("#navbarOptions");
-    navbarOptions.className = "flex items-center gap-12";
+
     mobileHamburger.classList.add("hidden");
+
+    navbarOptions.className = "flex items-center gap-12";
+
+    ofertaElements.forEach((element) => element.classList.remove("pl-10"));
+
+    ofertaDropdown.classList.remove("-left-10");
+    ofertaDropdown.classList.remove("-right-10");
+    ofertaDropdown.classList.add("left-[-50%]");
   }
 };
 
 toggleMobile();
 
 window.addEventListener("resize", () => {
-  console.log("toggle!");
   toggleMobile();
 });
-
-const navbarOptions = navbar.querySelector("#navbarOptions");
-
-const mobileContainer = document.createElement("div");
 
 setupHomePage();
